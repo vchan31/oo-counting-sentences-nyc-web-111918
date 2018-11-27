8cb444124c1741c5f885d18e8437af56b24c0298
Vincent Chan <vchan313@gmail.com>
2018-11-27 14:16:40 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,42 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
-  def count_sentences
+def count_sentences
 
-  end
+count = 0
+array = self.split(" ")
+
+
+array.each do |word|
+if word[-1, 1] == "."
+   count += 1
+end
+end #end loop
+ 
+ array.each do |word|
+ if word[-1, 1] == "!"
+   count += 1
+end
+end #end loop
+ 
+array.each do |word|
+ if word[-1, 1] == "?"
+   count += 1
+end
+end #end loop
+ 
+count
+ end # end function 
+ 
 end
